80ee1b9050d6950ac1eac1b1768e7a88646621e5
rexy3e-tech <rexy3e@gmail.com>
2026-08-19 00:38:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,12 @@
 
 # Discord Bot Token
 # Get from: https://discord.com/developers/applications
-DISCORD_TOKEN = "MTUwMTgzMTc5ODYxNzkzMTkwMA.GpZMb9.nS5YGSNde4QLI9LGR778lxRmBeyejRXBNuUzdc"
+DISCORD_TOKEN = "" #Put token here
 
 # Spotify API Credentials (for Spotify links)
 # Get from: https://developer.spotify.com/dashboard → Create App
-SPOTIFY_CLIENT_ID     = "fed23a5f694b43919346e1bfdededa08"
-SPOTIFY_CLIENT_SECRET = "8c408329d5114503b531066e8ee494b5"
+SPOTIFY_CLIENT_ID     = ""
+SPOTIFY_CLIENT_SECRET = ""
 
 # Bot Settings
 BOT_PREFIX   = "!"
